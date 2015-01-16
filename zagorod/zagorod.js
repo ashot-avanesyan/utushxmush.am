2ac2d0ba9ce7bd008ffb3d459ab19152a5f138b7
Armine Hayrapetyan <hayrapet.armine@gmail.com>
2015-01-16 14:03:20 +0000

    Author: Arman_Armine Revisions: add javascript function

--- zagorod/zagorod.js
+++ zagorod/zagorod.js
@@ -7,5 +7,21 @@
                        mapTypeId:google.maps.MapTypeId.ROADMAP
                      };
          var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-        }
+       
          google.maps.event.addDomListener(window, 'load', initialize);
+        }
+
+/* Arman_Armine */
+/* make a Calculator */
+function Calculator()
+{
+	var totalXmichq = 0; 
+	var c1 = document.getElementById('c1');
+
+	if ( c1.checked )
+		totalXmichq += 5000;
+}
+
+
+/*print function */ 
+
