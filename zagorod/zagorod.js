a9a70ed1e441d1581c402e86075bbfdd048fa376
Ashot Avanesyan <ashot.avanesyan05@gmail.com>
2015-01-23 09:05:23 +0000

    Author: Erik create menu tab

--- zagorod/zagorod.js
+++ zagorod/zagorod.js
@@ -94,11 +94,27 @@
 	}
 
 
+//Author: Erik change hide 1_2 
+	function hide10_2() {
+                document.getElementById("d2_2").style.display = "none";
+                document.getElementById("d2_3").style.display = "none";
+                document.getElementById("d2_4").style.display = "none";
+                document.getElementById("d2_5").style.display = "none";
+                document.getElementById("d2_1").style.display = "block";
+	}
 	function hide10_4() {
+                document.getElementById("d2").style.display = "none";
+                document.getElementById("d2_1").style.display = "none";
                 document.getElementById("d2_2_1_2").style.display = "none";
                 document.getElementById("d2_2_1_3").style.display = "none";
-                document.getElementById("d2_2_1_4").style.display = "none";
-                document.getElementById("d2_2_1_1").style.display = "block";
+	}
+//Author: Erik change hide20_2 
+        function hide20_2() {
+                document.getElementById("d2_1").style.display = "none";
+                document.getElementById("d2_3").style.display = "none";
+                document.getElementById("d2_4").style.display = "none";
+                document.getElementById("d2_5").style.display = "none";
+                document.getElementById("d2_2").style.display = "block";
 	}
         function hide20_4() {
                 document.getElementById("d2_2_1_1").style.display = "none";
@@ -106,17 +122,41 @@
                 document.getElementById("d2_2_1_4").style.display = "none";
                 document.getElementById("d2_2_1_2").style.display = "block";
 	}
+//Author: Erik change hide30_2 
+        function hide30_2() {
+                document.getElementById("d2_1").style.display = "none";
+                document.getElementById("d2_2").style.display = "none";
+                document.getElementById("d2_4").style.display = "none";
+                document.getElementById("d2_5").style.display = "none";
+                document.getElementById("d2_3").style.display = "block";
+	}
 	function hide30_4() {
                 document.getElementById("d2_2_1_1").style.display = "none";
                 document.getElementById("d2_2_1_2").style.display = "none";
                 document.getElementById("d2_2_1_4").style.display = "none";
                 document.getElementById("d2_2_1_3").style.display = "block";
 	}
+//Author: Erik change hide40_2 
+        function hide40_2() {
+                document.getElementById("d2_1").style.display = "none";
+                document.getElementById("d2_2").style.display = "none";
+                document.getElementById("d2_3").style.display = "none";
+                document.getElementById("d2_5").style.display = "none";
+                document.getElementById("d2_4").style.display = "block";
+	}
 	function hide40_4() {
                 document.getElementById("d2_2_1_1").style.display = "none";
                 document.getElementById("d2_2_1_2").style.display = "none";
                 document.getElementById("d2_2_1_3").style.display = "none";
                 document.getElementById("d2_2_1_4").style.display = "block";
+	}
+//Author: Erik change hide50_2 
+        function hide50_2() {
+                document.getElementById("d2_1").style.display = "none";
+                document.getElementById("d2_2").style.display = "none";
+                document.getElementById("d2_3").style.display = "none";
+                document.getElementById("d2_4").style.display = "none";
+                document.getElementById("d2_5").style.display = "block";
 	}
  
  
