f10b84b41a49455c1e764b9890ab2b560f10ae4e
Armine Hayrapetyan <hayrapet.armine@gmail.com>
2015-01-30 11:50:36 +0000

    Author: Armine Revisions: added initialize functions for barbecue

--- zagorod/zagorod.js
+++ zagorod/zagorod.js
@@ -108,16 +108,162 @@ function initializeBarbecue(name)
 	switch(name)
 	{
 		case pig:
-			document.getElementById("kgPig").value = 5;
-			document.getElementById("drPig").value = 3800;
-			break;
+			if (document.getElementById("pig").checked == true)	
+			{
+				document.getElementById("kgPig").value = 5;
+				document.getElementById("drPig").value = 3800;
+				document.getElementById("drPig").disabled = false;
+				document.getElementById("kgPig").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgPig").value = "";
+				document.getElementById("drPig").value = "";
+				document.getElementById("drPig").disabled = true;
+				document.getElementById("kgPig").disabled = true;
+					
+			} 
+            break;
+		
 		case lamb:
-			document.getElementById("kgLamb").value = 5;
-			document.getElementById("drLamb").value = 3000;
-			break;
+			if (document.getElementById("lamb").checked == true)	
+			{
+				document.getElementById("kgLamb").value = 5;
+				document.getElementById("drLamb").value = 3000;
+				document.getElementById("drLamb").disabled = false;
+				document.getElementById("kgLamb").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgLamb").value = "";
+				document.getElementById("drLamb").value = "";
+				document.getElementById("drLamb").disabled = true;
+				document.getElementById("kgLamb").disabled = true;
+			} 
+            break;
 		case goat:
-			document.getElementById("kgGoat").value = 5;
-			document.getElementById("drGoat").value = 3500;
+			if (document.getElementById("goat").checked == true)	
+			{
+				document.getElementById("kgGoat").value = 5;
+				document.getElementById("drGoat").value = 3500;
+				document.getElementById("drGoat").disabled = false;
+				document.getElementById("kgGoat").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgGoat").value = "";
+				document.getElementById("drGoat").value = "";
+				document.getElementById("drGoat").disabled = true;
+				document.getElementById("kgGoat").disabled = true;
+			} 
+            break;
+        case chicken:
+			if (document.getElementById("chicken").checked == true)	
+			{
+				document.getElementById("kgChicken").value = 5;
+				document.getElementById("drChicken").value = 1600;
+				document.getElementById("drChicken").disabled = false;
+				document.getElementById("kgChicken").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgChicken").value = "";
+				document.getElementById("drChicken").value = "";
+				document.getElementById("drChicken").disabled = true;
+				document.getElementById("kgChicken").disabled = true;
+			} 
+            break;
+	    case wing:
+			if (document.getElementById("wing").checked == true)	
+			{
+				document.getElementById("kgWing").value = 5;
+				document.getElementById("drWing").value = 3800;
+				document.getElementById("drWing").disabled = false;
+				document.getElementById("kgWing").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgWing").value = "";
+				document.getElementById("drWing").value = "";
+				document.getElementById("drWing").disabled = true;
+				document.getElementById("kgWing").disabled = true;
+			} 
+            break;
+	    case haunch:
+			if (document.getElementById("haunch").checked == true)	
+			{
+				document.getElementById("kgHaunch").value = 5;
+				document.getElementById("drHaunch").value = 3800;
+				document.getElementById("drHaunch").disabled = false;
+				document.getElementById("kgHaunch").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgHaunch").value = "";
+				document.getElementById("drHaunch").value = "";
+				document.getElementById("drHaunch").disabled = true;
+				document.getElementById("kgHaunch").disabled = true;
+			} 
+            break;
+	    case potatoes:
+			if (document.getElementById("potatoes").checked == true)	
+			{
+				document.getElementById("kgPotatoes").value = 5;
+				document.getElementById("drPotatoes").value = 3800;
+				document.getElementById("drPotatoes").disabled = false;
+				document.getElementById("kgPotatoes").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgPotatoes").value = "";
+				document.getElementById("drPotatoes").value = "";
+				document.getElementById("drPotatoes").disabled = true;
+				document.getElementById("kgPotatoes").disabled = true;
+			} 
+            break;
+		case anvil: 
+			if (document.getElementById("anvil").checked == true)	
+			{
+				document.getElementById("kgAnvil").value = 5;
+				document.getElementById("drAnvil").value = 3800;
+				document.getElementById("drAnvil").disabled = false;
+				document.getElementById("kgAnvil").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgAnvil").value = "";
+				document.getElementById("drAnvil").value = "";
+				document.getElementById("drAnvil").disabled = true;
+				document.getElementById("kgAnvil").disabled = true;
+			} 
+            break;
+		case internal_organs:
+			if (document.getElementById("internal_organs").checked == true)	
+			{
+				document.getElementById("kgInternal_organs").value = 5;
+				document.getElementById("drInternal_organs").value = 3800;
+				document.getElementById("drInternal_organs").disabled = false;
+				document.getElementById("kgInternal_organs").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("kgInternal_organs").value = "";
+				document.getElementById("drInternal_organs").value = "";
+				document.getElementById("drInternal_organs").disabled = true;
+				document.getElementById("kgInternal_organs").disabled = true;
+			} 
+            break;
+		case etc: 
+			if (document.getElementById("etc").checked == true)	
+			{
+				document.getElementById("drEtc").disabled = false;
+				document.getElementById("kgEtc").disabled = false;
+			}
+			else 
+			{
+				document.getElementById("drEtc").disabled = true;
+				document.getElementById("kgEtc").disabled = true;
+			}
 			break;
 	   default: 
 			window.alert("This is default value.");
